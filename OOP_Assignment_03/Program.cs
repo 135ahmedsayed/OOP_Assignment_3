1a121bb547b139f5cd550c0eec26db2abf6ba7fb
135ahmedsayed <ahmedshkra246@gmail.com>
2025-07-13 22:03:36 +0300

    Solve Part1_Q1

--- OOP_Assignment_03/Program.cs
+++ OOP_Assignment_03/Program.cs
@@ -4,8 +4,28 @@
     {
         static void Main(string[] args)
         {
-
+            #region Q1
+            Console.WriteLine("The Sum is " + Add(10, 90));          // Add(int , int) => 100
+            Console.WriteLine("The Sum is " + Add(10, 20, 50));       // Add(int , int , int) => 80
+            Console.WriteLine("The Sum is " + Add(1.1, 0.1) + "\n"); // Add(double , double) => 1.2
+            #endregion
             Console.ReadLine();
         }
+        #region With part(1)
+        #region Q1
+        public static int Add(int Num1, int Num2)
+        {
+            return Num1 + Num2;
+        }
+        public static int Add(int Num1, int Num2, int Num3)
+        {
+            return Num1 + Num2 + Num3;
+        }
+        public static double Add(double Num1, double Num2)
+        {
+            return Num1 + Num2;
+        }
+        #endregion
+        #endregion
     }
 }
